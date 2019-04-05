efdfec431b431856798a45fea9055f446e36cde8
h22121 <h22121@lab.cpcc.edu>
2019-04-04 20:29:23 -0400

    classwork 829pm

--- person.js
+++ person.js
@@ -1,8 +1,8 @@
-class Employee {                      // creating a class, every class will inherit Object
+class Person {                      // creating a class, every class will inherit Object
     constructor(firstName, lastName, age) {     //  when i say newEmployee i'm executing this method
-        this._firstName = firstName;            //      "
-        this._lastName = lastName;              //      "
-        this._age = age;                        //     "
+        this.firstName = firstName;            //      "
+        this.lastName = lastName;              //      "
+        this.age = age;                        //     "
     }                                           //     "
     set firstName(newFirstName) {
         this._firstName = newFirstName;
@@ -11,7 +11,7 @@ class Employee {                      // creating a class, every class will inhe
         return this._firstName;
     }
     set lastName(newLastName) {
-        this._lastNae = newLastName;
+        this._lastName = newLastName;
     }
     set age(newAge) {
         if (!isNaN(newAge)) {
@@ -20,26 +20,12 @@ class Employee {                      // creating a class, every class will inhe
         else {
             this._age = 30;
         }
-    } 
-        get age(){
-            return this._age;
-        }
     }
-var employee1 = new Employee('Brandon', 'Smith', 47);    // key word NEW will tell JS to execute this method
-var employee2 = {     // last week created an object this way, very slow
-    firstName: 'Laura',
-    lastName: 'Long'
+    get age() {
+        return this._age;
+    }
 }
-function Log(a) {
-    console.log(a);
-}
-employee1.firstName = 'Brent';
-employee1.age = 'Forty-Seven';
 
-console.log(employee1);
-console.log(employee2);
+module.exports = Person
 
-console.log(employee1.toString());      // did not define toString earlier but can call that method 
-//can do this by inheritance
-console.log(employee1 instanceof Object);  // results in TRUE
-console.log(Log instanceof Object);        //Log is a function and also an object
+
